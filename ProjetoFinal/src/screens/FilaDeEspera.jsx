41c7274c4102e430acdd725b1d11825d8469d8c7
AlineRodrigues02 <alinemariarodrigues08@gmail.com>
2025-07-07 22:59:58 -0300

    Adicionado o local storage para a fila de espera

--- ProjetoFinal/src/screens/FilaDeEspera.jsx
+++ ProjetoFinal/src/screens/FilaDeEspera.jsx
@@ -1,8 +1,29 @@
+import { useContext } from "react";
+import { PacienteContext } from "./LocalStorage";
+
 function FilaDeEspera() {
+  const {pacientes} = useContext(PacienteContext);
+  const getStatus = (p) =>{
+    if (p.atendido) return "✅ Em Atendimento"
+    if (p.triagem) return "👨‍⚕️ Aguardando Consulta"
+    return "📝 Aguardando Triagem"
+  }
   return (
-    <div class="container">
+    <div className="container">
       <h1>Fila de Espera</h1>
-      <div class="filaDeEspera" id="filaDeEspera"></div>
+      <div className="painel">
+        {pacientes.length ===0 ?(
+          <p>Nenhum paciente cadastrado</p>
+        ):(
+          pacientes.map((paciente, index)=>(
+            <div key={index} className={`painel-card ${paciente.triagem?.risco || ''}`}>
+              <p><strong>Nome:</strong>{paciente.nome}</p>
+              <p><strong>Status:</strong>{getStatus(paciente)}</p>
+              </div>
+          ))
+        )}
+      </div>
+      <div className="filaDeEspera" id="filaDeEspera"></div>
     </div>
   );
 }
