701625bd2c6a7d0612afa9f2b8a44ef826dc4e57
AlineRodrigues02 <alinemariarodrigues08@gmail.com>
2025-07-16 14:26:35 -0300

    Adiconei uma imagem para tv para classificação de risco

--- ProjetoFinal/src/screens/FilaDeEspera.jsx
+++ ProjetoFinal/src/screens/FilaDeEspera.jsx
@@ -2,32 +2,38 @@ import { useContext } from "react";
 import { PacienteContext } from "./LocalStorage";
 import "../Styles/FiladeEspera.css"
 function FilaDeEspera() {
-  const {pacientes} = useContext(PacienteContext);
-  
-  const getStatus = (p) =>{
+  const { pacientes } = useContext(PacienteContext);
+
+  const getStatus = (p) => {
     if (p.atendido) return "✅ Atendimento Finalizado"
     if (p.emAtendimento) return "✅ Em Atendimento"
     if (p.triagem) return "👨‍⚕️ Aguardando Consulta"
     return "📝 Aguardando Triagem"
   }
-  
-  return (
-    <div className="container">
-      <h1>Fila de Espera</h1>
-      <div className="painel">
-        {pacientes.length ===0 ?(
-          <p>Nenhum paciente cadastrado</p>
-        ):(
-          pacientes.map((paciente, index) => (
 
-            <div key={index} className="painel-card">
-              <p><strong>Nome:</strong>{paciente.nome}</p>
-              <p><strong>Status:</strong>{getStatus(paciente)}</p>
-              </div>
-          ))
-        )}
+  return (
+    <div className="container tv-layout">
+      <div className="painel-imagem">
+        <img src="../public/Riscos.jpg" alt="Classificação de risco" />
       </div>
-      <div className="filaDeEspera" id="filaDeEspera"></div>
+
+      <div className="painel-lista">
+        <h1>Fila de Espera</h1>
+        <div className="painel">
+          {pacientes.length === 0 ? (
+            <p>Nenhum paciente cadastrado</p>
+          ) : (
+            pacientes.map((paciente, index) => (
+
+              <div key={index} className="painel-card">
+                <p><strong>Nome:</strong>{paciente.nome}</p>
+                <p><strong>Status:</strong>{getStatus(paciente)}</p>
+              </div>
+            ))
+          )}
+        </div>
+      </div>
+      {/* <div className="filaDeEspera" id="filaDeEspera"></div> */}
     </div>
   );
 }
